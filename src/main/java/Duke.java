dae265afe5c0d07506025b4905917e13d4f2be92
Reinbowl <marcusngyong@gmail.com>
2020-08-15 22:27:05 +0800

    Level-2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,8 @@ public class Duke {
         String line = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
         Scanner Input = new Scanner(System.in);
         String userInput;
+        String[] Tasks = new String[100];
+        int numOfTasks = 0;
 
         System.out.println(line);
         System.out.println("                Hewwo! I'm UwU Bot                 \n"
@@ -16,12 +18,23 @@ public class Duke {
 
         while(!(userInput = Input.nextLine()).equals("bye")) {
             System.out.println(line);
-            System.out.println(userInput);
+            switch (userInput) {
+                case "list": //list tasks
+                    for(int i = 1; i <= numOfTasks; i++) {
+                        System.out.println(i + ". " + Tasks[i-1]);
+                    }
+                    break;
+                default:
+                    //System.out.println(userInput); //echo
+                    Tasks[numOfTasks] = userInput;
+                    numOfTasks++;
+                    System.out.println("added: " + userInput);
+            }
             System.out.println(line);
         }
 
+        System.out.println(line);
         System.out.println("Bai Bai! See ywo swoon again!");
         System.out.println(line);
-
     }
 }
