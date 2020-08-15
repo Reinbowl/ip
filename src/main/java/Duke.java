1a957159f79c5ebadda4b2b12ea2bd9f0422473f
Reinbowl <marcusngyong@gmail.com>
2020-08-15 18:03:24 +0800

    Level-1: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,27 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String line = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
+        Scanner Input = new Scanner(System.in);
+        String userInput;
 
-        System.out.println("―――――――――――――――――――――――――――――――――");
-        System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
-        System.out.println("―――――――――――――――――――――――――――――――――");
+        System.out.println(line);
+        System.out.println("                Hewwo! I'm UwU Bot                 \n"
+                          +"               （✿ ͡◕ ᴗ◕)つ━━✫・*。                  ");
+        System.out.println(line);
 
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("―――――――――――――――――――――――――――――――――");
+        System.out.println("What can I dwo fwor ywu?");
+        System.out.println(line);
+
+        while(!(userInput = Input.nextLine()).equals("bye")) {
+            System.out.println(line);
+            System.out.println(userInput);
+            System.out.println(line);
+        }
+
+        System.out.println("Bai Bai! See ywo swoon again!");
+        System.out.println(line);
+
     }
 }
